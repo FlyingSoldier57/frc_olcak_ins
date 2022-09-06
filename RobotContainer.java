c402d1013c54d9639f89b814bf3328774a4a9ff1
FlyingSoldier57 <98491440+FlyingSoldier57@users.noreply.github.com>
2022-09-06 12:21:37 +0300

    Update RobotContainer.java

--- RobotContainer.java
+++ RobotContainer.java
@@ -11,15 +11,11 @@ import frc.robot.Constants.PS4ControllerButton;
 
 
 
-
- 
- 
-
   public class RobotContainer {
     
     PS4Controller drivController = new PS4Controller(DriveConstants.Drive_Controller);
-	  PS4Controller operatorController = new PS4Controller(DriveConstants.OPERATOR_CONTROLLER);
-	  PS4Controller kSquare = new PS4Controller(PS4ControllerButton.BUTTON_SQUARE);
+    PS4Controller operatorController = new PS4Controller(DriveConstants.OPERATOR_CONTROLLER);
+    PS4Controller kSquare = new PS4Controller(PS4ControllerButton.BUTTON_SQUARE);
     PS4Controller kTriangle = new PS4Controller(PS4ControllerButton.BUTTON_TRIANGLE);
     PS4Controller kCross = new PS4Controller(PS4ControllerButton.BUTTON_CROSS);
     PS4Controller kCircle = new PS4Controller(PS4ControllerButton.BUTTON_CIRCLE);
@@ -32,10 +28,6 @@ import frc.robot.Constants.PS4ControllerButton;
     PS4Controller kShare = new PS4Controller(PS4ControllerButton.BUTTON_SHARE);
     PS4Controller kPS = new PS4Controller(PS4ControllerButton.BUTTON_PS);
     PS4Controller kTouchPad = new PS4Controller(PS4ControllerButton.BUTTON_TOUCHPAD);
-    
-    
-  
-    
 
     public double GetDriverRawAxis(int axis){
       return drivController.getRawAxis(axis); }
