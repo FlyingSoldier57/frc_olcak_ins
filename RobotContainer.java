699f7c0893b09c92ff6e24b4843335ad5b247cc9
FlyingSoldier57 <98491440+FlyingSoldier57@users.noreply.github.com>
2022-09-17 07:57:28 +0300

    Update RobotContainer.java

--- RobotContainer.java
+++ RobotContainer.java
@@ -13,24 +13,7 @@ import frc.robot.Constants.PS4ControllerButton;
 
   public class RobotContainer {
     
-    PS4Controller drivController = new PS4Controller(DriveConstants.Drive_Controller);
-    PS4Controller operatorController = new PS4Controller(DriveConstants.OPERATOR_CONTROLLER);
-    PS4Controller kSquare = new PS4Controller(PS4ControllerButton.BUTTON_SQUARE);
-    PS4Controller kTriangle = new PS4Controller(PS4ControllerButton.BUTTON_TRIANGLE);
-    PS4Controller kCross = new PS4Controller(PS4ControllerButton.BUTTON_CROSS);
-    PS4Controller kCircle = new PS4Controller(PS4ControllerButton.BUTTON_CIRCLE);
-    PS4Controller kL1pPs4Controller = new PS4Controller(PS4ControllerButton.BUTTON_L1);
-    PS4Controller kR2 = new PS4Controller(PS4ControllerButton.BUTTON_R2);
-    PS4Controller kR3 = new PS4Controller(PS4ControllerButton.BUTTON_R3);
-    PS4Controller kR1 = new PS4Controller(PS4ControllerButton.BUTTON_R1);
-    PS4Controller kL2 = new PS4Controller(PS4ControllerButton.BUTTON_L2);
-    PS4Controller kL3 = new PS4Controller(PS4ControllerButton.BUTTON_L3);
-    PS4Controller kShare = new PS4Controller(PS4ControllerButton.BUTTON_SHARE);
-    PS4Controller kPS = new PS4Controller(PS4ControllerButton.BUTTON_PS);
-    PS4Controller kTouchPad = new PS4Controller(PS4ControllerButton.BUTTON_TOUCHPAD);
-
-    public double GetDriverRawAxis(int axis){
-      return drivController.getRawAxis(axis); }
+    
 
   // The robot's subsystems and commands are defined here...
 
